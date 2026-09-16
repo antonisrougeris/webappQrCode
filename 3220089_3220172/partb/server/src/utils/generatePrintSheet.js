6b561f62296d69097978e677d16c9725e34d6552
antonisrougeris <150190755+antonisrougeris@users.noreply.github.com>
2026-09-16 16:19:41 +0000

    fix dimention

--- 3220089_3220172/partb/server/src/utils/generatePrintSheet.js
+++ 3220089_3220172/partb/server/src/utils/generatePrintSheet.js
@@ -13,8 +13,8 @@ const A3_WIDTH = 3508;
 const A3_HEIGHT = 4961;
 
 // Εδώ αλλάζεις τις πραγματικές διαστάσεις εκτύπωσης
-const QR_WIDTH_CM = 26;
-const LOGO_WIDTH_CM = 7;
+const QR_WIDTH_CM = 27;
+const LOGO_WIDTH_CM = 6;
 const NECK_LABEL_WIDTH_CM = 4.5;
 
 // Κενά ανάμεσα στα στοιχεία (pixels)
